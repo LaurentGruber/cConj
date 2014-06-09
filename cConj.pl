27224993ee4bd85d3e01d46d2ec66a166211762b
LaurentG <laurent@sk1.be>
2014-06-09 20:40:25 +0200

    add claroline connect link

--- cConj.pl
+++ cConj.pl
@@ -18,6 +18,7 @@ my $conf = LoadFile('config/config.yml');
 my $TaggerBin = $conf->{TaggerBin};
 my $TaggerSeparator = $conf->{TaggerSeparator};
 my $TagFile = $conf->{TagFile};
+my $claco = $conf->{claco};
 my $db = $conf->{db};
 my $serveur = $conf->{serveur};
 my $login = $conf->{login};
@@ -32,6 +33,8 @@ $dbcon->do('set names utf8');
 #Récupère la structure des tags
 my $Tag = LoadFile($TagFile);
 
+#Récupère l'exercice (Claco) auquel associer la question
+my $exerciceId = 1;
 
 #Récupère le fichier passé en argument et l'ouvre
 my $file = $ARGV[0];
@@ -55,6 +58,7 @@ my $nbVerbePhrase = 0;
 my $verbe;
 my $temps;
 my $reponse;
+my $req;
 
 foreach my $ligne (@lignes) {
     my @exp = split (/$TaggerSeparator/, $ligne);
@@ -68,7 +72,10 @@ foreach my $ligne (@lignes) {
             $verbe .= $exp[2].",";
             $temps .= $Tag->{Verbe}->{$exp[1]}.",";
             $reponse .= $exp[0].",";
-            $mots .= " [Verbe] ";
+            if ($claco == 1){
+                $mots .= qq( <input id="1" class="blank" name="blank_1" size="25" type="text" value="[REPONSE]" /> [$exp[2] - $Tag->{Verbe}->{$exp[1]}] );
+            }
+            else{ $mots .= " [Verbe] ";}
 
         }
         else {
@@ -77,9 +84,46 @@ foreach my $ligne (@lignes) {
     }
     else {
 		$phrase = $mots.".";
-		print $phrase." -> ".$verbe." -> ".$temps." -> ".$reponse." -> ".$nbVerbePhrase."\n"; # split sur , ensuite boucle selon nbVerbePhrase pour choper les diff verbe
-        my $req = qq(INSERT INTO PhraseEx(Phrase, Verbe, Temps, Reponse, TitreTexte) VALUES("$phrase", "$verbe", "$temps", "$reponse", "$titreTexte"));
-        $dbcon->do($req) or die "Echec requete $req : $DBI::errstr";
+        if ($claco == 1){
+            #Crée une question
+            $req = qq(INSERT INTO ujm_question(user_id, category_id, title, locked, model, date_create) VALUES (1, 1, "$titreTexte", 0, 0, NOW()));
+            $dbcon->do($req) or die "Echec requete $req : $DBI::errstr";
+            my $question_id = $dbcon->{mysql_insertid};
+            #Associe une question avec un exercice
+            $req = qq(INSERT INTO ujm_exercise_question(exercise_id, question_id, ordre) VALUES ($exerciceId, $question_id, $question_id));
+            $dbcon->do($req) or die "Echec requete $req : $DBI::errstr";
+            #Crée une interaction
+            $req = qq(INSERT INTO ujm_interaction(question_id, type, invite) VALUES ("$question_id", "InteractionHole", "<p>compl&egrave;te le texte</p>"));
+            $dbcon->do($req) or die "Echec requete $req : $DBI::errstr";
+            my $interaction_id = $dbcon->{mysql_insertid};
+            #Crée le contenu de la question
+            my @reponses = split (/,/, $reponse);
+            my $phrasehtml = "<p>".$phrase."</p>";
+            foreach $verbe (@reponses){
+                $phrasehtml =~ s/\[REPONSE\]/$verbe/;
+            }
+            my $phrasehtmlw = "<p>".$phrase."</p>";
+            $phrasehtmlw =~ s/\[REPONSE\]//g;
+            $req = $dbcon->prepare(qq(INSERT INTO ujm_interaction_hole(interaction_id, html, htmlWithoutValue) VALUES ($interaction_id, ?, ?)));
+            $req->execute($phrasehtml, $phrasehtmlw);
+            my $interaction_hole_id = $dbcon->{mysql_insertid};
+            #Crée les réponses
+            my $rep ="";
+            foreach $verbe (@reponses){
+                $rep = $verbe;
+                $req = qq(INSERT INTO ujm_hole (interaction_hole_id, size, position, orthography, selector) VALUES ($interaction_hole_id, 25, 1, 0, 0));
+                $dbcon->do($req) or die "Echec requete $req : $DBI::errstr";
+                my $hole_id = $dbcon->{mysql_insertid};
+                $req = qq(INSERT INTO ujm_word_response(hole_id, response, score) VALUES ($hole_id, "$rep", 1));
+                $dbcon->do($req) or die "Echec requete $req : $DBI::errstr";
+            }
+        }
+        else {
+            #Insert dans la base de donnée
+            $req = qq(INSERT INTO PhraseEx(Phrase, Verbe, Temps, Reponse, TitreTexte) VALUES("$phrase", "$verbe", "$temps", "$reponse", "$titreTexte"));
+            $dbcon->do($req) or die "Echec requete $req : $DBI::errstr";
+        }
+		print " Ajout du/des verbe(s) ".$verbe." à conjuguer au(x) temps suivant :".$temps."\n";
         $mots = "";
         $verbe = "";
         $temps = "";
